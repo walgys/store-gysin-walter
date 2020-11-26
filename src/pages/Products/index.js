949cdb5ad97cce2fac0496681cb0202917e849bd
Walter A Gysin <gysinwalter@gmail.com>
2020-11-26 17:28:32 -0300

    cleanes unused

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useCallback} from 'react'
+import React, { useContext, useEffect} from 'react'
 import ProductsContainer from '../../containers/ProductsContainer';
 import {appContext} from '../../contexts'
 import {endpoint} from '../../utils'
--- src/pages/RedeemHistory/index.js
+++ src/pages/RedeemHistory/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useCallback } from 'react'
+import React, { useEffect, useContext} from 'react'
 import  HistoryContainer from '../../containers/HistoryContainer/'
 import { appContext } from '../../contexts'
 import { endpoint } from '../../utils'
